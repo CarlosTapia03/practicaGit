eca2b11033eabedfcccb21209abcf1448e0d1370
Carlos Tapia <carlosviril3@gmail.com>
2022-12-09 09:14:51 +0100

    metodos clase gatos

--- src/Gatos.java
+++ src/Gatos.java
@@ -9,6 +9,23 @@ public class Gatos {
 		this.nombre = nombre;
 		this.edad = edad;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
 	
 	
 	
