7c62d120a688f527c38c12c0bf142f31de0d3f44
Carlos Tapia <carlosviril3@gmail.com>
2022-12-09 09:16:21 +0100

    metodos clase carlos

--- src/Carlos.java
+++ src/Carlos.java
@@ -4,4 +4,18 @@ public class Carlos {
 	private String nombre;
 	private int edad;
 	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
 }
